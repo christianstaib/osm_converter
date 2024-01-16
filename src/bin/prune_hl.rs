6e5dbe508e922000708bff1dc635abdc7ce3ce47
Christian Staib <staib.christian@hotmail.de>
2024-01-16 21:12:20 +0100

    cleaned file

--- src/bin/prune_hl.rs
+++ src/bin/prune_hl.rs
@@ -1,14 +1,12 @@
 use std::{
     fs::File,
     io::{BufReader, BufWriter},
-    time::{Duration, Instant},
+    time::Instant,
 };
 
 use clap::Parser;
-use indicatif::ProgressIterator;
-use osm_test::routing::{hl::hub_graph::HubGraph, route::RouteValidationRequest};
+use osm_test::routing::hl::hub_graph::HubGraph;
 
-/// Starts a routing service on localhost:3030/route
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
@@ -18,16 +16,10 @@ struct Args {
     /// Path of .fmi file
     #[arg(short, long)]
     pruned_hub_graph: String,
-    /// Path of .fmi file
-    #[arg(short, long)]
-    test_path: String,
 }
 
 fn main() {
     let args = Args::parse();
-
-    let reader = BufReader::new(File::open(args.test_path.as_str()).unwrap());
-    let tests: Vec<RouteValidationRequest> = serde_json::from_reader(reader).unwrap();
 
     let reader = BufReader::new(File::open(args.hub_graph).unwrap());
     let mut hub_graph: HubGraph = bincode::deserialize_from(reader).unwrap();
@@ -38,20 +30,4 @@ fn main() {
 
     let writer = BufWriter::new(File::create(args.pruned_hub_graph).unwrap());
     bincode::serialize_into(writer, &hub_graph).unwrap();
-
-    let mut time_hl = Vec::new();
-    tests.iter().progress().for_each(|test| {
-        let start = Instant::now();
-        let cost = hub_graph.get_cost(&test.request);
-        time_hl.push(start.elapsed());
-
-        assert_eq!(cost, test.cost);
-    });
-
-    println!("all correct");
-
-    println!(
-        "took {:?} per search",
-        time_hl.iter().sum::<Duration>() / time_hl.len() as u32
-    );
 }
