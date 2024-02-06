e46afdc73614ac94a41d70e47a809716b1b673df
Christian Staib <staib.christian@hotmail.de>
2024-02-06 12:06:47 +0100

    did stuff

--- src/bin/create_hl.rs
+++ src/bin/create_hl.rs
@@ -7,8 +7,7 @@ use std::{
 use clap::Parser;
 
 use osm_test::routing::{
-    ch::contractor::ContractedGraph, hl::hub_graph::HubGraph,
-    simple_algorithms::ch_bi_dijkstra::ChDijkstra,
+    ch::contractor::ContractedGraph, simple_algorithms::ch_bi_dijkstra::ChDijkstra,
 };
 
 /// Starts a routing service on localhost:3030/route
@@ -35,8 +34,12 @@ fn main() {
     let dijkstra = ChDijkstra::new(&contracted_graph);
 
     let start = Instant::now();
-    let hub_graph = HubGraph::new(&dijkstra, args.hop_limit);
+    let mut hub_graph = dijkstra.get_hl();
     println!("Generating hl took {:?}", start.elapsed());
+
+    let start = Instant::now();
+    hub_graph.set_predecessor();
+    println!("set predecessor took {:?}", start.elapsed());
 
     let writer = BufWriter::new(File::create(args.hub_graph).unwrap());
     bincode::serialize_into(writer, &hub_graph).unwrap();
